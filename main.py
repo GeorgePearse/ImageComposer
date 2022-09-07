2acca2e0d6c7de3cfb48916a80cbb71b884e7599
George Pearse <47161914+GeorgePearse@users.noreply.github.com>
2022-09-07 15:46:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,25 @@ import cv2
 import cvzone 
 from matplotlib import pyplot as PLT
 
-def overlay(foreground_path, background_path): 
+def flexible_overlay(background_path, foreground_path, target_x, target_y):
+    """
+    target_x: higher number = further to the right
+    target_y: higher number = further down
+    """
+    imgBack = cv2.imread(background_path)
+    imgFront = cv2.imread(foreground_path, cv2.IMREAD_UNCHANGED)
+    imgFront = cv2.resize(imgFront, (0, 5), None, 0.04, 0.04)
 
-    background = cv2.imread(background_path)
-    foreground = cv2.imread(foreground_path, cv2.IMREAD_UNCHANGED)
 
-    height, width, _ = foreground.shape
-    dim = (width, height)
+    hf, wf, cf = imgFront.shape
+    hb, wb, cb = imgBack.shape
 
-    # resize image
-    resized_background = cv2.resize(background, dim, interpolation = cv2.INTER_AREA)
-
-    result = cvzone.overlayPNG(resized_background, foreground, [0,0])
+    result = cvzone.overlayPNG(imgBack, imgFront, 
+                               #[target_x, hb-hf])
+                               [target_x, target_y])
+    return result
+    
+    
+def plot_composed_image(result):
     PLT.imshow(result)
     PLT.show()
