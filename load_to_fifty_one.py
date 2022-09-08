a630abca871e832d4e410927b8069001d61c9937
George Pearse <47161914+GeorgePearse@users.noreply.github.com>
2022-09-08 16:14:22 +0100

    Update load_to_fifty_one.py

--- load_to_fifty_one.py
+++ load_to_fifty_one.py
@@ -1,35 +1,32 @@
 import glob
 import fiftyone as fo
 
-images_patt = "/path/to/images/*"
 
-# Ex: your custom label format
-annotations = {
-    "/path/to/images/000001.jpg": [
-        {"bbox": ..., "label": ...},
-        ...
-    ],
-    ...
-}
+annotated_images = [annotated_image]
 
 # Create samples for your data
 samples = []
-for filepath in glob.glob(images_patt):
-    sample = fo.Sample(filepath=filepath)
+for annotated_image in annotated_images:
+    
+    sample = fo.Sample(filepath=annotated_image.path)
 
     # Convert detections to FiftyOne format
     detections = []
-    for obj in annotations[filepath]:
-        label = obj["label"]
+    
+    bounding_box = annotated_image.bounding_box
+    
+    #for bounding_box in annotated_image.bounding_boxes:
+    
+    label = bounding_box.label
 
-        # Bounding box coordinates should be relative values
-        # in [0, 1] in the following format:
-        # [top-left-x, top-left-y, width, height]
-        bounding_box = obj["bbox"]
+    # Bounding box coordinates should be relative values
+    # in [0, 1] in the following format:
+    # [top-left-x, top-left-y, width, height]
+    bounding_box = bounding_box.return_fifty_one_style()
 
-        detections.append(
-            fo.Detection(label=label, bounding_box=bounding_box)
-        )
+    detections.append(
+        fo.Detection(label=label, bounding_box=bounding_box)
+    )
 
     # Store detections in a field name of your choice
     sample["ground_truth"] = fo.Detections(detections=detections)
@@ -37,5 +34,5 @@ for filepath in glob.glob(images_patt):
     samples.append(sample)
 
 # Create dataset
-dataset = fo.Dataset("my-detection-dataset")
+dataset = fo.Dataset("augmented-samples")
 dataset.add_samples(samples)
