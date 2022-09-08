0ba66376f0b0718b827daa5022c5e8a353ed1347
George Pearse <47161914+GeorgePearse@users.noreply.github.com>
2022-09-08 10:09:28 +0100

    Update augmentations.py

--- augmentations.py
+++ augmentations.py
@@ -15,3 +15,17 @@ image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 # Augment an image
 transformed = transform(image=image)
 transformed_image = transformed["image"]
+
+
+###########################################
+
+def add_alpha_channel(img):
+    """
+    Sometimes a png image doesn't have an alpha channel.
+    
+    Most of the code you're writing needs an alpha channel
+    """
+    b_channel, g_channel, r_channel = cv2.split(img)
+    alpha_channel = np.ones(b_channel.shape, dtype=b_channel.dtype) * 50 #creating a dummy alpha channel image.
+    img_BGRA = cv2.merge((b_channel, g_channel, r_channel, alpha_channel))
+    return img_BGRA
